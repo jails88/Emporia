0749c63d880cf6a641ee1a515c99f17f099cb4b0
Jails <iulius_borcea@hotmail.com>
2017-09-26 20:05:41 +0200

    Revert "Added Google Login"
    This reverts commit 08a8ba7be6e0f544daedf57b5c3c9e2327fa68bd.

--- prototype/app/src/main/java/com/example/julius/prototype/LoginActivity.java
+++ prototype/app/src/main/java/com/example/julius/prototype/LoginActivity.java
@@ -1,11 +1,8 @@
 package com.example.julius.prototype;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -17,25 +14,15 @@ import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener  {
+public class LoginActivity extends AppCompatActivity {
 
     LoginButton loginButton;
     CallbackManager callbackManager;
-    SignInButton SignIn;
-    GoogleApiClient googleApiClient;
-    private static final int REQ_CODE = 9001;
 
 
     @Override
@@ -74,12 +61,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
             }
         });
-
-        SignIn = (SignInButton) findViewById(R.id.google_button);
-        SignIn.setOnClickListener(this);
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
-
     }
 
     public void displayUserInfo(JSONObject object)
@@ -110,53 +91,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQ_CODE)
-        {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleResult(result);
-        } else {
-            callbackManager.onActivityResult(requestCode, resultCode, data);
-        }
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
-    @Override
-    public void onClick(View v) {
-        signIn();
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    private void signIn()
-    {
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(intent, REQ_CODE);
-
-    }
-
-    private void handleResult(GoogleSignInResult result)
-    {
-        if(result.isSuccess())
-        {
-            GoogleSignInAccount account = result.getSignInAccount();
-            TextView tv_name, tv_email, tv_id;
-            tv_name = (TextView)findViewById(R.id.tv_name);
-            tv_email = (TextView)findViewById(R.id.tv_email);
-            tv_id = (TextView)findViewById(R.id.tv_id);
-
-            tv_name.setText(account.getDisplayName());
-            tv_email.setText("Email: "+ account.getEmail());
-            tv_id.setText("ID:" + account.getId());
-        }
-
-    }
-
-    private void isLoggedIn(boolean isLoggedIn)
-    {
-
-    }
-
 }
